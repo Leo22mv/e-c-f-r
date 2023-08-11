10bbd5039a4395375ac4199097ac054e8a2daedb
Kbza <g.leonel.m17@gmail.com>
2023-08-11 09:18:27 -0300

    0.1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <img src='./../assets/img/logo.png' />
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <nav className="navbar bg-dark" data-bs-theme="dark">
+            <ul className='text-white ml-5'>
+                <li>
+                    <a href='#'>Inicio</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
